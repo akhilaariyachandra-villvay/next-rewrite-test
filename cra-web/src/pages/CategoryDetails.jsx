3a6f825d579698ce8e0802e5d0695abd8dd410c9
Akhila Ariyachandra <akhila.ariyachandra@villvay.com>
2023-11-22 15:40:53 +0530

    Try loading image from codebase

--- cra-web/src/pages/CategoryDetails.jsx
+++ cra-web/src/pages/CategoryDetails.jsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import placeholderImage from "../assets/placeholder.webp";
 
 const CategoryDetailsPage = () => {
   const { id } = useParams();
@@ -6,6 +7,18 @@ const CategoryDetailsPage = () => {
   return (
     <>
       <h1 className="title">Category {id}</h1>
+
+      <div>
+        {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
+        <img
+          src={placeholderImage}
+          alt="Placeholder image"
+          width={171}
+          height={171}
+        />
+
+        <p>This image is being loaded directly from the CRA code base.</p>
+      </div>
     </>
   );
 };
